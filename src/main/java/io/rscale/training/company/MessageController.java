68dc087560255eb7783c96c34718cd5afee2ff4a
dawg2k3 <elkarao@gmail.com>
2017-09-13 10:51:35 -0700

    pointing to config server

---
+++ src/main/java/io/rscale/training/company/MessageController.java
@@ -0,0 +1,18 @@
+package io.rscale.training.company;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class MessageController {
+
+	@Value("${message:Hello default}")
+    private String message;
+
+    @RequestMapping("/message")
+    String getMessage() {
+        return "\n" + this.message + "\n\n";
+    }
+}
